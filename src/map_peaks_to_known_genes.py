82171ea215104abaec5f8a2db40f972c4a1ebf11
Alexander Lenail <zfrenchee@gmail.com>
2017-01-31 14:43:45 -0500

    Moved code to src folder, added empty README

--- src/map_peaks_to_known_genes.py
+++ src/map_peaks_to_known_genes.py
@@ -1,7 +1,9 @@
 
-
+from itertools import groupby
 from csv import DictReader, DictWriter
 from optparse import OptionParser
+
+from intervaltree import Interval, IntervalTree
 
 
 
@@ -26,7 +28,7 @@ parser.add_option('--symbol-xref',dest='symbol_xref',default=None,help='Provide 
 
 
 
-def parse_known_genes_file(file):
+def parse_known_genes_file(file_object):
 	"""
 
 
@@ -50,26 +52,49 @@ def parse_known_genes_file(file):
 
 	known_genes_file_fieldnames = ["name","chrom","strand","txStart","txEnd","cdsStart","cdsEnd","exonCount","exonStarts","exonEnds","proteinID","alignID"]
 
-	reader = csv.DictReader(file, delimiter='\t', fieldnames=column_names)
+	known_genes_file_reader = DictReader(file_object, delimiter='\t', fieldnames=known_genes_file_fieldnames)
 
 
-def parse_peaks_file(file):
+def parse_peaks_file(file_object):
 	"""
 	My contract is that I will return to you an instance of Peaks, independent of the filetype you supply me
+
+	BED: 
+	
+	chrom - The name of the chromosome (e.g. chr3, chrY, chr2_random) or scaffold (e.g. scaffold10671).
+	chromStart - The starting position of the feature in the chromosome or scaffold. The first base in a chromosome is numbered 0.
+	chromEnd - The ending position of the feature in the chromosome or scaffold. The chromEnd base is not included in the display of the feature. For example, the first 100 bases of a chromosome are defined as chromStart=0, chromEnd=100, and span the bases numbered 0-99.
+
+	The 9 additional optional BED fields are:
+
+	name - Defines the name of the BED line. This label is displayed to the left of the BED line in the Genome Browser window when the track is open to full display mode or directly to the left of the item in pack mode.
+	score - A score between 0 and 1000. If the track line useScore attribute is set to 1 for this annotation data set, the score value will determine the level of gray in which this feature is displayed (higher numbers = darker gray). This table shows the Genome Browser's translation of BED score values into shades of gray:
+	strand - Defines the strand. Either "." (=no strand) or "+" or "-".
+	thickStart - The starting position at which the feature is drawn thickly (for example, the start codon in gene displays). When there is no thick part, thickStart and thickEnd are usually set to the chromStart position.
+	thickEnd - The ending position at which the feature is drawn thickly (for example, the stop codon in gene displays).
+	itemRgb - An RGB value of the form R,G,B (e.g. 255,0,0). If the track line itemRgb attribute is set to "On", this RBG value will determine the display color of the data contained in this BED line. NOTE: It is recommended that a simple color scheme (eight colors or less) be used with this attribute to avoid overwhelming the color resources of the Genome Browser and your Internet browser.
+	blockCount - The number of blocks (exons) in the BED line.
+	blockSizes - A comma-separated list of the block sizes. The number of items in this list should correspond to blockCount.
+	blockStarts - A comma-separated list of block starts. All of the blockStart positions should be calculated relative to chromStart. The number of items in this list should correspond to blockCount.
+
+	MACS
+	GPS
+
 	"""
+	
+	peaks_file_fieldnames = ["chrom","chromStart","chromEnd","name","score","strand","thickStart","thickEnd","itemRgb","blockCount","blockSizes","blockStarts"]
+
+	peaks_file_reader = DictReader(file_object, delimiter='\t', fieldnames=peaks_file_fieldnames)
 
 
 
-
-
-
-def parse_kgXref_file(file):
+def parse_kgXref_file(file_object):
 	"""
 	"""
 
 	kgXref_file_fieldnames = ['kgID','mRNA','spID','spDisplayID','geneSymbol','refseq','protAcc','description']
 
-	reader = csv.DictReader(file, delimiter='\t', fieldnames=column_names)
+	kgXref_reader = DictReader(file_object, delimiter='\t', fieldnames=kgXref_file_fieldnames)
 
 
 
@@ -91,14 +116,45 @@ def main():
 		kgXref = parse_kgXref_file(kgXref_file)
 
 
+	reference = group_by_chromosome(reference)
+	peaks = group_by_chromosome(peaks)
 
 
+	reference = {chrom: IntervalTree_from_dict(genes) for chrom, genes in reference}
+
+
+	map = map_peaks_to_reference(peaks, reference)
+
+	output(map)
+
+
+
+
+def group_by_chromosome(dictionary):
+	"""
+	"""
+
+
+
+
+def IntervalTree_from_dict(dictionary):
+	"""
+	"""
+
+
+
+class Peaks():
+	"""
+	"""
+	def __init__(self, arg):
+		self.arg = arg
+		
 
 
 if __name__ == '__main__' :
 	main()
 
 
-
+########################################### Error Logic ###########################################
 
 
