857e6155198b68af843a0a8a80bee9957f9c007d
Alexander Lenail <zfrenchee@gmail.com>
2017-03-11 21:21:40 -0500

    Fixed some bugs

--- src/testing.py
+++ src/testing.py
@@ -10,24 +10,27 @@ class Options:
 	def __init__(self, options):
 		self.__dict__.update(options)
 
-peaks = "../example/wgEncodeUWDukeDnaseA549.fdr01peaks.hg19.bed"
-# reference = "../data/ucsc_hg19_knownGenes.tsv"
-# motifs = "../data/HUMAN_hg19_BBLS_1_00_FDR_0_10.bed"
 
-kgXref = "../data/ucsc_hg19_kgXref.tsv"
-expression = "../example/Tgfb_exp.tsv"
-
-# peaks = "../data/peaks_IntervalTree_dictionary.pickle"
+# reference = "../data/ucsc_hg19_knownGenes.tsv" # was used to create
 reference = "../data/reference_IntervalTree_dictionary.pickle"
-motifs = "../data/motifs_IntervalTree_dictionary.pickle"
+kgXref = "../data/ucsc_hg19_kgXref.tsv"
 
-# peaks = "../example/A549_FOXA1_broadPeak.bed"
+# motifs = "../data/multiz46way_placental.txt" # was used to create
+motifs = "../data/motifs_IntervalTree_dictionary.pickle"
+# motifs = "../data/HUMAN_hg19_BBLS_1_00_FDR_0_10.bed"
+# motifs = "../data/FOXA1_motifs.tsv"
 # motifs = "../example/foxa1.motifs"
+
+peaks = "../example/wgEncodeUWDukeDnaseA549.fdr01peaks.hg19.bed"
+# peaks = "../example/A549_FOXA1_broadPeak.bed"
+# peaks = "../example/JUNB_ChIP_A549.bed"
+# peaks = "../example/CTCF_ChIP_A549.bed"
+
+expression = "../example/Tgfb_exp.tsv"
 
 options = Options({"upstream_window": 2000,"downstream_window": 2000,"tss": False,"kgXref_file": kgXref,"output_dir":'/Users/alex/Documents/GarNet2/example/'})
 
 
-output(map_known_genes_to_peaks(reference, peaks, options), options.output_dir)
+output(map_motifs_to_peaks(motifs, peaks, options), options.output_dir)
 
 # output(TF_regression(df, expression, options), options.output_dir)
-
