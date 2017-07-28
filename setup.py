2b869475251b8c67ca0b1af3c8656acbc2cc6336
Alexander Lenail <zfrenchee@gmail.com>
2017-07-28 11:01:43 -0400

    Update directory scheme

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ from setuptools import setup
 setup(
     name='GarNet',
     packages=['GarNet'],
-    package_dir={'GarNet': 'src'},
     package_data={'GarNet': ['summary.jinja']},
-    version='0.4.3',
+    version='0.4.5',
     url='https://github.com/fraenkel-lab/GarNet',
     classifiers=[
         'Programming Language :: Python :: 3.5',
@@ -22,8 +21,5 @@ setup(
         'intervaltree',
         'jinja2'
     ],
-    entry_points={
-        'console_scripts': [ 'GarNet = src.__main__:main' ]
-    },
 )
 
